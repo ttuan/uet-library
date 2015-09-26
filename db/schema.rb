eeb8e524f6d18d00093f3a3c3a2a384cad146b04
ttuan <tuantv.nhnd@gmail.com>
2015-09-26 12:29:55 +0700

    add image background

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150926040545) do
+ActiveRecord::Schema.define(version: 20150925031219) do
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
